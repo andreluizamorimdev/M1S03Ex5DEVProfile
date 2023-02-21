a0c91291a8c91f4e7423516dc2b5fd1ed16d29bf
andreluizamorimdev <andreluizamorim.dev@gmail.com>
2023-02-20 23:20:29 -0300

    Modified css and added some github api data

--- functions.js
+++ functions.js
@@ -1,6 +1,10 @@
 let profileAvatar = document.getElementById('avatar');
 let profileName = document.getElementById('profileName');
 let profileBio = document.getElementById('profileBio');
+let profileEmail = document.getElementById('profileEmail');
+let profileRepos = document.getElementById('profileRepos');
+let profileGithubUrl = document.getElementById('profileGithubUrl');
+let profileTwitter = document.getElementById('profileTwitter');
 
 async function getProfileGithub() {
     const response = await fetch('https://api.github.com/users/andreluizamorimdev');
@@ -9,5 +13,14 @@ async function getProfileGithub() {
     profileAvatar.src = data?.avatar_url;
     profileName.innerHTML = data?.name;
     profileBio.innerHTML = data?.bio;
+    if(data?.email != null ) {
+        profileEmail.innerHTML = data?.email;
+    } else {
+        profileEmail.innerHTML = "andreluizamorim.dev@gmail.com";
+    }
+    profileRepos.innerHTML = data?.public_repos
+    profileGithubUrl.href = data?.html_url
+    profileTwitter.href = `https://www.twitter.com/${data?.twitter_username}`
+
 }
 getProfileGithub();
